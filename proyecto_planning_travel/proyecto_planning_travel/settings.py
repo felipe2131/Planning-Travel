9fe77131d858fa660133cc17e872bce95924c5b6
Andres Felipe Marulanda <fv2961125@gmail.com>
2024-05-29 06:57:59 -0500

    first commt

--- proyecto_planning_travel/proyecto_planning_travel/settings.py
+++ proyecto_planning_travel/proyecto_planning_travel/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
